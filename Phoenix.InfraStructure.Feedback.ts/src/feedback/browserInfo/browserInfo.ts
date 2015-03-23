73d08f7e6459541b1e36adf30a73757cbd8824ac
Mohammad <dot.dotbydot@gmail.com>
2015-03-23 23:07:57 +0430

    Add plugins information in browserInfo.ts add moduletag,convastag,helpertag in feedbackcontent add construstor in feedbackContent class add default value of feedbackContent class in feedbackOption constructor

--- Phoenix.InfraStructure.Feedback.ts/src/feedback/browserInfo/browserInfo.ts
+++ Phoenix.InfraStructure.Feedback.ts/src/feedback/browserInfo/browserInfo.ts
@@ -1,13 +1,16 @@
-﻿     class browserInfo {
-        appCodeName: string = navigator.appCodeName;
-        appName: string = navigator.appName;
-        appVersion: string = navigator.appVersion;
-        cookieEnabled: boolean = navigator.cookieEnabled;
-        onLine: boolean = navigator.onLine;
-        platform: string = navigator.platform;
-        userAgent: string = navigator.userAgent;
-        static getInformation(): browserInfo {
-            return new this;
+﻿class browserInfo {
+    appCodeName: string = navigator.appCodeName;
+    appName: string = navigator.appName;
+    appVersion: string = navigator.appVersion;
+    cookieEnabled: boolean = navigator.cookieEnabled;
+    onLine: boolean = navigator.onLine;
+    platform: string = navigator.platform;
+    userAgent: string = navigator.userAgent;
+    plugins: Array<string>;
+    static getInformation(): browserInfo {
+        for (var plugin in navigator.plugins) {
+            this.prototype.plugins.push(plugin.name);
         }
+        return new this;
     }
-
+}
--- Phoenix.InfraStructure.Feedback.ts/src/feedback/feedback.ts
+++ Phoenix.InfraStructure.Feedback.ts/src/feedback/feedback.ts
@@ -2,32 +2,32 @@
 /// <reference path="../jquery.d.ts" />
 
 module phoenix {
-    export class feedbackContent {
-        description: any = "";
-        highlighter: any = "";
-        overview: any = "";
-        submitSuccess: any = "";
-        submitFailor: any = "";
+    class feedbackContent {
+        constructor(public description: any, public highlighter: any, public overview: any, public submitSuccess: any, public submitFailor: any) { }
+        private moduleTag: any = '<div id="fb-module" position="absolute" left="0px" top="0px">';
+        private convasTag: any = '<canvas dir= "rtl" id="fb-canvas" style="z-index=999999" width="' + this.documentWidth + '" height="' + this.documentHeight + '"></canvas>';
+        private helperTag: any = '<div id="fb-helpers"></div>';
+        private noteTag: any = '<input id="fb-note" name="fb-note" type="hidden"></div>';
+        public documentHeight: number = window.innerHeight;
+        public documentWidth: number = window.innerWidth;
+        public getFeedbackTemplate(): any {
+            return this.moduleTag + this.description
+                + this.highlighter + this.overview + this.convasTag
+                + this.helperTag + this.noteTag;
+        }
     }
     export class feedbackOptions extends feedbackContent {
         constructor(public url: string,
             public onStart: any, public onClose: any,
-            feedbackContent: feedbackContent) {
-            super();
-            this.description = feedbackContent.description;
-            this.highlighter = feedbackContent.highlighter;
-            this.overview = feedbackContent.overview;
-            this.submitSuccess = feedbackContent.submitSuccess;
-            this.submitFailor = feedbackContent.submitFailor;
+            contentTemplate: feedbackContent = new feedbackContent("", "", "", "", "")) {
+            super(contentTemplate.description, contentTemplate.highlighter, contentTemplate.overview, contentTemplate.submitSuccess, contentTemplate.submitFailor);
         }
         highlightCounter: number = 1;
-        documentHeight: number = window.innerHeight;
-        documentWidth: number = window.innerWidth;
-        html2ConvasSupport: boolean = !!window.HTMLCanvasElement;
+        html2ConvasSupport: boolean = true; //!!window.HTMLCanvasElement; FIXME
     }
     export class feedback {
         private _postData: browserInfo = browserInfo.getInformation();
-        constructor(private $element: string,private fbOptions: feedbackOptions) {
+        constructor(private $element: string, private fbOptions: feedbackOptions) {
             $("#" + $element).on("click",(event: JQueryEventObject) => this.openFeedback(event));
         }
         public openFeedback(event: JQueryEventObject): void {
